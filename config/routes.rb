087883820cf2bf25ebb5bc41462c3ea803c7b9f8
Emily Baird <emilym.baird@gmail.com>
2022-01-15 14:49:42 +0000

    all mandatory complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#all"
+  get "tasks/:id", to: "tasks#find", as: :task
+  get "new", to: "tasks#new", as: :new
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
